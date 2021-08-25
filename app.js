f45b686aa415d48d97c810797a116fa4ee772227
Kotadiya Yash <yashkotadiya9008@gmail.com>
2021-08-25 22:15:42 +0530

    up to input output

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
-console.log(txtInput)
+var outputDiv = document.querySelector("#output")
+// outputDiv.innerText = "there are yassk available";
+
+// console.log(outputDiv)
 
 function clickHandler() {
-    console.log("Clicked!");
-    console.log("Input", txtInput.value)
+    outputDiv.innerText = "bayabaya " + txtInput.value;
+    // console.log("Clicked!");
+    // console.log("Input", txtInput.value)
 };
 
 
